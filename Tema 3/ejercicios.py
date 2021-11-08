efe9aec9e4250b0dfbab68bc65222dc75e72b021
eherraiz <eherraiz@myuax.com>
2021-11-08 14:57:45 +0100

    ld

--- Tema 3/ejercicios.py
+++ Tema 3/ejercicios.py
@@ -5,8 +5,17 @@ import os
 import random
 import re
 import sys
-print(dict(os.environ))
-# fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+import pprint
+
+# Get the list of user's
+# environment variables
+env_var = os.environ
+
+# Print the list of user's
+# environment variables
+print("User's Environment variable:")
+pprint.pprint(dict(env_var), width = 1)
 
 class Matrix:
     _n = 0
