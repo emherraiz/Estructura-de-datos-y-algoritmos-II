b2768fee4b72814e3b698722504b6e8bc807cf21
eherraiz <eherraiz@myuax.com>
2021-11-09 16:12:54 +0100

    probado

--- Tema 3 - Tipo examen/3---Ejercicio-tipo-examen-main/prueba.py
+++ Tema 3 - Tipo examen/3---Ejercicio-tipo-examen-main/prueba.py
@@ -1,10 +1,16 @@
 class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+    def __init__(mysillyobject, name, age):
+        mysillyobject.name = name
+        mysillyobject.age = age
 
-    def myfunc(self):
-        print("Hello my name is " + self.name)
+    def myfunc(abc):
+        print("Hello my name is " + abc.name)
 
-p1 = Person("John", 36)
+p1 = Person("pepa", 36)
 p1.myfunc()
+
+import re
+
+txt = "The rain in Spain"
+x = re.search("^The.*Spain$", txt)
+print(x)
